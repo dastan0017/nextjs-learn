a30475a3166226683825ef7a6df22cbdaf5488dc
d.rakhamnazhov <dastan.rakhamnazhov@timelysoft.net>
2021-09-21 16:33:35 +0600

    Make a starter components and add navigation with routing

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function About() {
+	return (
+		<div>
+			<Head>
+				<title>About NextJS</title>
+				<meta
+					name="keywords"
+					content="NextJS and React development, programming"
+				/>
+			</Head>
+			<h1>from About page</h1>
+		</div>
+	);
+}
